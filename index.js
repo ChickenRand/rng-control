def2a8327de880358f14db7ba377a5f127157b16
Andréas Livet <andreas.livet@stormshield.eu>
2017-04-01 13:21:17 +0200

    Eslint the code

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-"use strict";
+'use strict';
 
 const app = require('express')();
 const request = require('request');
 const WebSocket = require('ws');
 
-const url = process.env.CHICKENRAND_URL || 'http://localhost:7000';
-const RNG_URL = process.env.RNG_URL || 'localhost:8080';
+const CHICKENRAND_URL = process.env.CHICKENRAND_URL || 'http://localhost:7000';
 const CONTROL_PASSWORD = process.env.CONTROL_PASSWORD || 'toto';
-const XP_DURATION = 10;
-const RNG_ID = 1;
-const j = request.jar();
+const RNG_URL = process.env.RNG_URL || 'localhost:8080';
+
+const NB_TRIALS_PER_SECOND = 10; // We recieve the message each 100ms
+const RNG_CONTROL_PORT = 1337;
+const RNG_ID = 1; // TODO : use rng rest API
+const XP_DURATION = 10; // in seconds
+const XP_TRIALS = NB_TRIALS_PER_SECOND * XP_DURATION;
+
+const COOKIE_JAR = request.jar();
 
 let results;
 let ws = null;
@@ -21,15 +26,15 @@ let trialsCount = 0;
 
 function resetResults() {
 	results = {
-		date : Date.now(),
-		trials : [],
+		date: Date.now(),
+		trials: [],
 		rng_control: true
 	};
 }
 
-function bitAt(byte, pos){
+function bitAt(byte, pos) {
 	return ((byte & (1 << pos)) !== 0);
-};
+}
 
 function connectingRng() {
 	console.log('Connecting to RNG');
@@ -41,41 +46,45 @@ function connectingRng() {
 		trialsCount = 0;
 	});
 
-	ws.on('message', (data, flags) => {
-		var trialRes = {
+	ws.on('message', data => {
+		const NB_BITS_PER_BYTE = 8;
+		const trialRes = {
 			numbers: Array.from(new Uint8Array(data)),
 			nbOnes: 0,
 			nbZeros: 0,
 			ms: Date.now() - results.date
 		};
-		for(var i = 0; i < trialRes.numbers.length; i++){
-			for(var pos = 0; pos < 8; pos++){
-				bitAt(trialRes.numbers[i], pos) ? trialRes.nbOnes++ : trialRes.nbZeros++;
+		trialRes.numbers.forEach(num => {
+			for (let pos = 0; pos < NB_BITS_PER_BYTE; pos++) {
+				if (bitAt(num, pos)) {
+					trialRes.nbOnes++;
+				} else {
+					trialRes.nbZeros++;
+				}
 			}
-		}
+		});
 		totalBits += trialRes.nbOnes + trialRes.nbZeros;
 		results.trials.push(trialRes);
 
 		trialsCount++;
-		// We recieve the message each 100ms
-		if(trialsCount > XP_DURATION * 10) {
+		if (trialsCount > XP_TRIALS) {
 			stopExperiment();
 		}
 	});
 }
 
 function startExperiment() {
-	if(queueId == null) {
+	if (queueId === null) {
 		console.log('No queueId');
 		return;
 	}
-	request.post(`${url}/queue/start/${queueId}.json`, {jar: j}, (err, httpResponse, body) => {
+	request.post(`${CHICKENRAND_URL}/queue/start/${queueId}.json`, {jar: COOKIE_JAR}, (err, httpResponse, body) => {
 		if (err) {
 			console.error('Error when starting the experiment.');
 			console.error(err);
 		}
 		const resp = JSON.parse(body);
-		if (resp.message != null) {
+		if (resp.message) {
 			console.error('problem');
 			return;
 		}
@@ -88,13 +97,13 @@ function stopExperiment() {
 		ws.close();
 		ws = null;
 	}
-	request.post(`${url}/queue/remove/${queueId}.json`, {jar: j}, (err, httpResponse, body) => {
+	request.post(`${CHICKENRAND_URL}/queue/remove/${queueId}.json`, {jar: COOKIE_JAR}, err => {
 		if (err) {
 			console.error('Error when leaving the queue.');
 			console.error(err);
 		}
 		queueId = null;
-		request.post(`${url}/xp/send_results/${xpId}`, {jar: j, form: {results: JSON.stringify(results), rng_id: RNG_ID, rng_control_user_id: userId}}, (err, httpResponse, body) => {
+		request.post(`${CHICKENRAND_URL}/xp/send_results/${xpId}`, {jar: COOKIE_JAR, form: {results: JSON.stringify(results), rng_id: RNG_ID, rng_control_user_id: userId}}, err => {
 			if (err) {
 				console.error('Error when sending experiment results.');
 				console.error(err);
@@ -110,7 +119,7 @@ function stopExperiment() {
 function addToQueue() {
 	// TODO : gérer le fait qu'on puisse déjà être dans la queue
 	// TODO : gérer la queue avec les states et tout
-	request.post(`${url}/queue/add/${xpId}.json`, {jar: j}, (err, httpResponse, body) => {
+	request.post(`${CHICKENRAND_URL}/queue/add/${xpId}.json`, {jar: COOKIE_JAR}, (err, httpResponse, body) => {
 		if (err) {
 			console.error('Error when entering into to the queue.');
 			console.error(err);
@@ -140,16 +149,15 @@ function createControlXp(req, res) {
 app.get('/rng-control', createControlXp);
 
 console.log('Log in to ChichenRand server as control@chickenrand.org');
-request.post(`${url}/user/login` , {jar: j, form: {email: 'control@chickenrand.org', password: CONTROL_PASSWORD}}, (err, httpResponse, body) => {
+request.post(`${CHICKENRAND_URL}/user/login`, {jar: COOKIE_JAR, form: {email: 'control@chickenrand.org', password: CONTROL_PASSWORD}}, err => {
 	if (err) {
 		console.error('Cannot log into chickenrand, exiting.');
-		console.error(err);
-		process.exit(1);
+		throw new Error(err);
 	}
 	console.log('Logged into chickenrand, waiting for control results to generate...');
 });
 
 
-app.listen(1337, () => {
-	console.log('Start server on port 1337');
-})
+app.listen(RNG_CONTROL_PORT, () => {
+	console.log(`Start server on port ${RNG_CONTROL_PORT}`);
+});
